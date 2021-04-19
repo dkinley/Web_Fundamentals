c81b36f2fe14bd634e93cd389678805f83122fd7
Dirk Kinley <42229326+dkinley@users.noreply.github.com>
2021-04-19 12:26:54 -0700

    Add files via upload

---
+++ loops-algo.jsx
@@ -0,0 +1,11 @@
+for(var num = 0; num < 20; num++){
+    if(num%2 != 0)
+    console.log(num);
+}
+
+var sum = 0
+for(var i = 1; i <= 5; i++){
+    sum = i + sum;
+    console.log("Number is " + i);
+    console.log("Sum is " + sum);
+    }
