8ae8cbd202884be4a95dafc1e7a13e5775ee6354
Dirk Kinley <42229326+dkinley@users.noreply.github.com>
2021-04-19 18:32:17 -0700

    Add files via upload

--- loops-algo.jsx
+++ loops-algo.jsx
@@ -1,11 +1,34 @@
-for(var num = 0; num < 20; num++){
-    if(num%2 != 0)
-    console.log(num);
+//Predict 1 - I predicted separate lines with 8,6,7,5,3,0,9
+var arr = [8,6,7,5,3,0,9]
+for(var i = 0; i < arr.length; i++){    
+    console.log(arr[i]);
 }
 
-var sum = 0
-for(var i = 1; i <= 5; i++){
-    sum = i + sum;
-    console.log("Number is " + i);
-    console.log("Sum is " + sum);
-    }
+//Predict 2 - I predicted separate lines with That is odd! That is odd!, 8, 4, 2, 0, That is odd!
+var arr = [7,3,8,4,2,0,1];
+for(var i = 0; i < arr.length; i++){ 
+    if(arr[i] % 2 == 0){
+        console.log(arr[i]);
+    } 
+    else{
+        console.log("That is odd!");
+    }
+}
+
+//Predict 3 I predicted arr to be [-1,-3,-8,5,Zero,2,-4,1] and newArr to be [-5,-2,-1]
+var arr = [1,3,8,-5,0,-2,4,-1];
+var newArr = [];
+for(var i = 0; i< arr.length; i++){
+    if(arr[i] < 0){
+        newArr.push(arr[i]);
+        arr[i] = arr[i] * -1;
+    }
+    else if(arr[i] == 0){
+        arr[i] = "Zero";
+    }
+    else{
+        arr[i] = arr[i] * -1;
+    }
+}
+console.log(arr);
+console.log(newArr);
