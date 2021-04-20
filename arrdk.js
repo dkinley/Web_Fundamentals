425c571a856a7f726ae412f1d97a829b60fc0e5e
Dirk Kinley <42229326+dkinley@users.noreply.github.com>
2021-04-20 10:10:38 -0700

    Delete arrdk.js

--- arrdk.js
+++
@@ -1,8 +0,0 @@
-var arrNew = [6,3,5,1,2,4];
-var sum = 0;
-for(var i = 0; i < arrNew.length; i++){
-    sum = sum + arrNew[i];
-    var prod = arrNew[i] * i;
-    console.log("Num " + arrNew[i] + ", Sum " + sum);
-    console.log("Value * Position is " + prod);
-}
